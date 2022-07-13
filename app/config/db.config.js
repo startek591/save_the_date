2ca3149ff9ff3ebf104e6037cd21a716c5c377e1
Jonathan Fambro <startek591@gmail.com>
2022-07-12 22:29:16 -0400

    Connected to a non-relational database

---
+++ app/config/db.config.js
@@ -0,0 +1,9 @@
+(function(){
+  const mongoose = require('mongoose');
+  const uri = process.env.MONGO_URI || 'mongodb://localhost:27017/save_the_date';
+  
+  mongoose.connect(uri).then(
+    () => { console.log(`Successfully connected to the database`); },
+    (error) => { console.log(`The database did not connect successfully. ${error}`)});
+
+}());
